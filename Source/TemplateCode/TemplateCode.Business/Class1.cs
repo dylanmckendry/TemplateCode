28865baa218662f213283034b997654ad702bb11
Dylan McKendry <dylan@thoughtstudio.com.au>
2012-09-15 21:21:59 +1000

    Starting work on repository
    Added some POCOs
    Working on a generic repository...

---
+++ Source/TemplateCode/TemplateCode.Business/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateCode.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/TemplateCode/TemplateCode.Repository/TemplateCodeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using TemplateCode.Model;
+
+namespace TemplateCode.Repository
+{
+    public class TemplateCodeContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
